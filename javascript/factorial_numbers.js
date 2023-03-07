62427cb0f0859f0742fa977a40f97d29430b6417
cjord01 <clayjchicago@gmail.com>
2023-03-07 13:13:33 -0600

    factorial_numbers for JS

---
+++ javascript/factorial_numbers.js
@@ -0,0 +1,13 @@
+// The factorial of a number is the product of all the positive integers that are less than or equal to the number in question. 
+// For example, for the number 6, the factorial would be 1 x 2 x 3 x 4 x 5 x 6 = 720.
+
+function factorialOf(number) {
+    var finalNum = 1;
+    for (var i = 1; i <= number; i++) {
+        finalNum *= i;
+    }
+    
+    console.log(finalNum);
+}
+
+factorialOf(6);
