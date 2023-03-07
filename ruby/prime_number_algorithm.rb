66b2d3eda728fa30b0b01e57dbecd61baa29a0ee
cjord01 <clayjchicago@gmail.com>
2023-03-07 14:03:13 -0600

    refactored ruby/prime_number_algorithm

--- ruby/prime_number_algorithm.rb
+++ ruby/prime_number_algorithm.rb
@@ -2,31 +2,29 @@
 
 def is_prime?(number)
   if number == 0 || number == 1
-    puts "#{number} is not prime"
-    return
+    return "#{number} is not prime"
   end
 
   for i in (2..number-1) do
     if (number % i) == 0
-      puts "#{number} is not prime"
-      return
+      return "#{number} is not prime"
     end
   end
 
-  puts "#{number} is prime"
+  return "#{number} is prime"
 end
 
-is_prime?(0)
-is_prime?(1)
-is_prime?(23)
-is_prime?(31)
-is_prime?(50)
-is_prime?(53)
-is_prime?(76)
-is_prime?(99)
-is_prime?(1000)
-is_prime?(1777)
-is_prime?(2500)
-is_prime?(5743)
-is_prime?(22225)
-is_prime?(28663)
+puts is_prime?(0)
+puts is_prime?(1)
+puts is_prime?(23)
+puts is_prime?(31)
+puts is_prime?(50)
+puts is_prime?(53)
+puts is_prime?(76)
+puts is_prime?(99)
+puts is_prime?(1000)
+puts is_prime?(1777)
+puts is_prime?(2500)
+puts is_prime?(5743)
+puts is_prime?(22225)
+puts is_prime?(28663)
