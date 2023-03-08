e421c487930c2d4980740f959974bcb4915203a3
cjord01 <clayjchicago@gmail.com>
2023-03-08 12:06:45 -0600

    check_for_repeating_digits for JS

---
+++ javascript/check_for_repeating_digits.js
@@ -0,0 +1,20 @@
+// Your objective is to validate if the number is made up of a series of the same digit in a row.
+
+// For example, the number 555 would qualify. A number with only two out of three digits repeating, such as 554, wouldn’t qualify.
+
+function repeatingDigits(number) {
+    if (Array.from(new Set(number.toString().split(''))).toString().length == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(repeatingDigits(22));
+console.log(repeatingDigits(34));
+console.log(repeatingDigits(444));
+console.log(repeatingDigits(556));
+console.log(repeatingDigits(7777));
+console.log(repeatingDigits(8989));
+console.log(repeatingDigits(11111));
+console.log(repeatingDigits(99998));
